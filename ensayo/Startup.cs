774332bd325a78dec326dd336720bd2b13849f6a
andrea-uribe <andrea.uribed@udea.edu.co>
2017-09-13 06:52:00 -0500

    Creación del proyecto

---
+++ ensayo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ensayo.Startup))]
+namespace ensayo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
